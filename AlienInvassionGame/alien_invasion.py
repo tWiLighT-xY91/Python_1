a987f5f278a912992e92206041091bdb3e1517e2
tWiLighT-kJ91 <twilightmorningstar32@gmail.com>
2025-01-03 12:58:01 +0000

    Made a different class -m

--- AlienInvassionGame/alien_invasion.py
+++ AlienInvassionGame/alien_invasion.py
@@ -2,14 +2,19 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
 
     def __init__(self):
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(self.settings.screen_width, self.settings.screen_height)
         pygame.display.set_caption("ALIEN INVASION")
+
+        
 
     def run_game(self):
 
@@ -17,6 +22,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            self.screen.fill(self.settings.bg_bgcolor)
 
             pygame.display.flip()
 
